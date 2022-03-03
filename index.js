6f17ed00b440769cf92bc1fbb399b1c56384d175
Александр <shura.sidorenko.2018@bk.ru>
2022-03-03 23:00:58 +0300

    asd

--- index.js
+++ index.js
@@ -7,36 +7,37 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.use(telegrafGetChatMembers)
 
 
-
 bot.on('message', async(ctx) => {
-    const user = await ctx.getChatMembers()[0].status;
     
-      if(user == 'member'){
+    let  user = ctx.getChatMembers()[0].status;
+    let idUser = ctx.getChatMembers()[0].user.id;
+//    console.log(ctx.message) 
+   
+
+      if(user !== 'creator' && user !== 'administrator' && idUser === ctx.message.from.id){
+
         try{
-        await ctx.reply(`${ctx.message.from.first_name}, вы не можете писать в данный чат 
+        await ctx.reply(`${ctx.message.from.username}, вы не можете писать в данный чат 
 Обратитесь к администратору`) 
         
          for(let i = -1; i <= 0; i++){
             await setTimeout(() => {
                ctx.deleteMessage(ctx.message.message_id-i) 
                 
-            },3000)
-            
-        }  
+            },1000 * 60)
+          }
+        
         }catch (err){
             console.log('error')
       }
-        
-          
-     
-        
-        
-        
-    
+
       } else{
           console.log('work')
+          
       }
+
 })
+
 
 
 
