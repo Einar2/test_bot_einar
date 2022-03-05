6dd8d5917ce4d4fe110e3b6738ab9e3067e7f245
Александр <shura.sidorenko.2018@bk.ru>
2022-03-05 21:02:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,44 +7,22 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.use(telegrafGetChatMembers)
 
 
-
-bot.on('message', async(ctx) => {
-    
-    let idUser = ctx.getChatMembers()[0].user.id;
-    let chatId = ctx.message.chat.id
-    let chatIds = `${ctx.message.chat.id}`
-    let fromId = ctx.message.from.id
-    let  user = telegrafGetChatMembers.all[`${chatId}`][`${fromId}`].status;
-//    console.log(ctx.message.chat.id)
-    
     
 
 
-   
-
-      if(user === 'member'  ){
-        try{
-        await ctx.reply(`@${ctx.message.from.username}, вы не можете писать в данный чат 
-Обратитесь к администратору`) 
-        
-         for(let i = -1; i <= 0; i++){
+bot.on('new_chat_members', async (ctx) => {
+   await ctx.reply(`@${ctx.message.from.username}, вы не можете писать в данный чат 
+//Обратитесь к администратору`) 
+    
+    for(let i = -1; i <= -1; i++){
             await setTimeout(() => {
                ctx.deleteMessage(ctx.message.message_id-i) 
                 
-            },1000 * 60)
+            },1000 * 15)
           }
-        
-        }catch (err){
-            console.log("err")
-      }
-
-      } 
-
+    await ctx.tg.restrictChatMember(ctx.message.chat.id, ctx.message.from.id, [false])
 })
 
-
-
- 
 
 
 
