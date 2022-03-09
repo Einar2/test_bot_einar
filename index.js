4ea89bc059c08e963ac8628ba24d597cf4e7f0d0
Einar2 <83465116+Einar2@users.noreply.github.com>
2022-03-09 18:12:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,34 +6,20 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.use(telegrafGetChatMembers)
 
-bot.on('message', async (ctx) => {
-    if(ctx.message.new_chat_member != undefined){
-        await ctx.reply(`@${ctx.message.from.username}, вы не можете писать в данный чат
-Обратитесь к администратору`)
-            for(let i = -1; i <= -1; i++){
-            await setTimeout(() => {
-               ctx.deleteMessage(ctx.message.message_id-i)
 
+bot.on('new_chat_members', async (ctx) => {
+   await ctx.reply(`@${ctx.message.from.username}, вы не можете писать в данный чат 
+Обратитесь к администратору`) 
+    console.log("Work")
+    
+    for(let i = -1; i <= -1; i++){
+            await setTimeout(() => {
+               ctx.deleteMessage(ctx.message.message_id-i) 
+                
             },1000 * 15)
           }
     await ctx.tg.restrictChatMember(ctx.message.chat.id, ctx.message.from.id, [false])
-    }
-}) 
-
-
-// bot.on('new_chat_member', async (ctx) => {
-//    await ctx.reply(`@${ctx.message.from.username}, вы не можете писать в данный чат 
-// Обратитесь к администратору`) 
-//     console.log("Work")
-    
-//     for(let i = -1; i <= -1; i++){
-//             await setTimeout(() => {
-//                ctx.deleteMessage(ctx.message.message_id-i) 
-                
-//             },1000 * 15)
-//           }
-//     await ctx.tg.restrictChatMember(ctx.message.chat.id, ctx.message.from.id, [false])
-// })
+})
 
 
 
